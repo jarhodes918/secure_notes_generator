d81d6018a105e672f7356c29237e3cfdb73c8b94
jarhodes918 <36420589+jarhodes918@users.noreply.github.com>
2018-02-24 03:17:45 -0500

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -5,16 +5,16 @@ module.exports = {
 	facebook: {
 		clientID: '1981344715416110',
 		clientSecret: 'e52c7012dceb9463e404fd56773714b3',
-		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+		callbackURL: 'http://ec2-18-219-75-35.us-east-2.compute.amazonaws.com:3000/oauth/facebook/callback'
 	},
 	twitter: {
 		clientID: 'Twitter Application ID',
 		clientSecret: 'Twitter Application Secret',
-		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+		callbackURL: 'http://ec2-18-219-75-35.us-east-2.compute.amazonaws.com:3000/oauth/twitter/callback'
 	},
 	google: {
 		clientID: 'Google Application ID',
 		clientSecret: 'Google Application Secret',
-		callbackURL: 'http://localhost:3000/oauth/google/callback'
+		callbackURL: 'http://ec2-18-219-75-35.us-east-2.compute.amazonaws.com:3000/oauth/google/callback'
 	}
 };
